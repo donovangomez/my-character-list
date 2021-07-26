40a3779efd9f993d88121d6d3c67364b0511eb63
Donovan <donovanjuniogomez@gmail.com>
2021-07-25 23:07:50 -0700

    add styled-components package

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,19 +1,27 @@
-import CharacterCard from './CharacterCard'
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  border: 2px solid blue;
+  display: flex;
+`;
 
 const MainContent = ({ characterList }) => {
-    return(
-        <div>
-            <h2>This is the main content</h2>
-            {characterList.map((character) => (
-                <CharacterCard 
-                  name={character.name}
-                  image={character.image_url}
-                  link={character.link}
-                  id={character.mal_id}
-                />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>This is the main content</h2>
+      <Wrapper>
+        {characterList.map((character) => (
+          <CharacterCard
+            name={character.name}
+            image={character.image_url}
+            link={character.link}
+            id={character.mal_id}
+          />
+        ))}
+      </Wrapper>
+    </div>
+  );
+};
 
 export default MainContent;
